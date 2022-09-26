f64eae9278edd5200c44bbf9386eb4901df53767
Asmaa Khan <asmaakhan41@gmail.com>
2022-09-25 20:29:44 -0400

    commit tree 2e78d5d37c91fd909f74fef8682bd5ad490aae6b

---
+++ index.js
@@ -0,0 +1,43 @@
+function add() {
+}
+
+function multiply() {
+
+}
+
+function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a,b) {
+    return a - b;
+  }
+
+  function multiply (a,b) {
+    return a * b;
+  }
+
+  function divide(a,b) {
+    return a / b;
+  }
+
+function increment(n) {
+    let number = n;
+    number++;
+    return number;
+}
+
+function decrement(n) {
+    let number = n;
+    number--;
+    return number;
+}
+
+function makeInt(n) {
+    let number = 10;
+}
+
+function makeInt(n) {
+    parseInt("n");
+    return "n";
+}
